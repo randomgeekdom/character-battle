9740183da27045fcde299f616ab2bd9557b7ae9c
randomgeekdom <randomgeekdom@gmail.com>
2022-09-18 14:59:18 -0500

    start of character battle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BattleComponent } from './battle/battle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BattleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
